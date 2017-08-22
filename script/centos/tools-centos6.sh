095b7adecd6616837eb95b1d4f548f3a7a5e5c78
ZnHoCn <zenudnju@gmail.com>
2017-08-22 23:50:58 +0800

    Updated: 2017-08-22 23:50:56

--- script/centos/tools-centos6.sh
+++ script/centos/tools-centos6.sh
@@ -5,4 +5,5 @@
 yum update -y
 yum install -y epel-release
 #rpm -ivh http://repository.it4i.cz/mirrors/repoforge/redhat/el6/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-yum install -y vim htop bash-com* lsof wget tar zip tree tmux
+yum install -y vim htop bash-com* lsof wget tar zip tree tmux python python-setuptools
+easy_install pip
