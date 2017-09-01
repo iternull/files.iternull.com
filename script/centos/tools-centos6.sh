0841624e18eb335ba460873f261f7e1b00c62ee8
ZnHoCn <zenudnju@gmail.com>
2017-09-01 15:00:43 +0800

    Updated: 2017-09-01 15:00:40

--- script/centos/tools-centos6.sh
+++ script/centos/tools-centos6.sh
@@ -5,5 +5,5 @@
 yum update -y
 yum install -y epel-release
 #rpm -ivh http://repository.it4i.cz/mirrors/repoforge/redhat/el6/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-yum install -y vim htop bash-com* lsof wget tar zip tree tmux python python-setuptools
+yum install -y vim man htop bash-com* lsof tar zip tree bc git wget rsync sysstat iftop iptraf ifstat netcat socat openssh-clients tmux python python-setuptools
 easy_install pip
--- script/others/rsvim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mv ~/.vimrc ~/vimrc_$(date --iso).bak
-cat << EOF > ~/.vimrc
-syntax on
-set nu
-set background=dark
-set tabstop=4
-EOF
