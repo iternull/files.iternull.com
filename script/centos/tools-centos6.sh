dbd06b184b3944ee4b2cdc4def0bce0d5ab17fb4
ZnHoCn <zenudnju@gmail.com>
2017-09-13 23:40:23 +0800

    Updated: 2017-09-13 23:40:20

--- script/centos/tools-centos6.sh
+++ script/centos/tools-centos6.sh
@@ -5,5 +5,14 @@
 yum update -y
 yum install -y epel-release
 #rpm -ivh http://repository.it4i.cz/mirrors/repoforge/redhat/el6/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-yum install -y vim man htop bash-com* lsof tar zip tree bc git wget rsync sysstat iftop iptraf ifstat netcat socat openssh-clients tmux python python-setuptools
-easy_install pip
+
+yum install -y vim man htop bash-com* lsof tar zip tree bc git wget rsync tmux
+yum install -y sysstat iftop iptraf ifstat netcat socat openssh-clients
+yum install -y python python-setuptools && easy_install pip
+
+yum install -y setuptool \
+ system-config-network* \
+ system-config-firewall* \
+ system-config-securitylevel-tui \
+ system-config-keyboard \
+ ntsysv
