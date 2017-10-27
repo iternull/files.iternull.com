4ef6c32c238a752237ff25f5cb34313a3bb9902e
ZnHoCn <zenudnju@gmail.com>
2017-10-27 20:40:49 +0800

    Updated: 2017-10-27 20:40:46

---
+++ script/debian/tools-debian8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## Copyright: IterNull
+## https://www.iternull.com/
+
+apt-get update && apt-get upgrade
+
+apt-get install -y vim man htop bash-com* lsof tar zip tree bc git wget rsync tmux
+apt-get install -y openssh-clients sysstat iftop iptraf ifstat netcat socat
+apt-get install -y python python-pip
