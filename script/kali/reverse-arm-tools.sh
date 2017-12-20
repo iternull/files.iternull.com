63290ed413418a1e775aa4f6ddef3ac1220c154b
ZnHoCn <me@hakr.xyz>
2017-12-20 23:37:00 +0800

    Updated: 2017-12-20 23:36:58

--- script/kali/reverse-arm-tools.sh
+++ script/kali/reverse-arm-tools.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y python python-pip minicom flashrom openocd usbprog binwalk xxd dexdump hexedit qemu qemu-efi-arm qemu-system-arm radare2 gcc gcc-arm-none-eabi gdb binutils binutils-arm-linux-gnueabi binutils-arm-linux-gnueabi-dbg binutils-arm-none-eabi
+apt-get install -y python python-pip minicom flashrom openocd usbprog avrdude
+apt-get install -y binwalk xxd dexdump hexedit
+apt-get install -y gcc gcc-arm-none-eabi gdb radare2 qemu
+apt-get install -y binutils binutils-arm-linux-gnueabi binutils-arm-linux-gnueabi-dbg binutils-arm-none-eabi
 
 pip install esptool ropgadget
