9ec674f88a9911abec858a515816f67ba2769559
ZnHoCn <me@hakr.xyz>
2018-07-28 19:59:24 +0800

    Updated: 2018-07-28 19:59:24

---
+++ script/pcapdump.sh
@@ -0,0 +1,21 @@
+##
+## Use:
+#    ./dump.sh                   Capture router wlan0 packet
+#    ./dump.sh date file_name    Rename UTC Timestamp to year-month-day-hour,minute,second
+
+# tcpdump -i wlan0 -w wlan0_$(date +%F-%H%M%S).cap
+# tcpflow -r wlan0_2018-01-01-081831.cap
+
+if [ "$1" == "date" ]; then
+    if [ -n "$2" ]; then
+#        mv $2 $(echo $2 | cut -d '_' -f 1)_$(date -d @`echo $2 | cut -d '_' -f 2 | head -c 10` +%Y-%m-%d-%H%M%S).cap
+        for i in `ls $2* | grep cap`; do echo $i; mv $i $(echo $i | cut -d _ -f 1)_$(date -d @`echo $i | cut -d _ -f 2 | cut -d . -f 1` +%Y-%m-%d-%H%M%S).cap; done
+    else
+#        for i in `ls *.cap`; do mv $i $(echo $i | cut -d _ -f 1)_$(date -d @`echo $i | cut -d _ -f 2 | head -c 10` +%Y-%m-%d-%H%M%S).cap; done
+        echo "No file name specified"
+    fi
+elif [ "$1" == "utc" ]; then
+    date -d @$2 +%Y-%m-%d-%H%M%S
+else
+    tcpdump -i wlan0 -w wlan0_$(date +%F-%H%M%S).cap
+fi
