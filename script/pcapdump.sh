ee17a21736e82225c433c33508da043c5c5448e0
ZnHoCn <me@hakr.xyz>
2018-07-28 20:17:30 +0800

    Updated: 2018-07-28 20:17:30

--- script/pcapdump.sh
+++ script/pcapdump.sh
@@ -1,10 +1,10 @@
 #
 ## Author: ZnHoCn
-##
-## Use:
-#    ./dump.sh                   Capture router wlan0 packet
-#    ./dump.sh date file_name    Rename UTC timestamp to year-month-day-hour,minute,second
-#    ./dump.sh utc 1532779430    Convert timestamp to readable format
+
+# Use:
+#   ./dump.sh                   Capture router wlan0 packet
+#   ./dump.sh date file_name    Rename UTC timestamp to year-month-day-hour,minute,second
+#   ./dump.sh utc 1532779430    Convert timestamp to readable format
 
 # tcpdump -i wlan0 -w wlan0_$(date +%F-%H%M%S).cap
 # tcpflow -r wlan0_2018-01-01-081831.cap
